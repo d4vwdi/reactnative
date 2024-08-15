afc290fb5a0b4e669eb417b992d8d862a38d2068
d4vwdi <d4vwdi@gmail.com>
2024-08-15 11:11:11 -0300

    Aula React Native

--- App.tsx
+++ App.tsx
@@ -1,118 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+// const FATEC = () => {
+//   return (
+//     <View>
+//       <Text>
+//         FATEC Zona Leste
+//       </Text>
+//     </View>
+//   );
+// };
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+// const ViewBoxWithColorAndText = () => {
+//   return (
+//     <View
+//     style={{
+//       flexDirection: 'row',
+//       height: 100,
+//       padding: 20
+//     }}>
+//     <View style={{ backgroundColor: 'blue', flex:0.2}}/>
+//     <View style={{ backgroundColor: 'green', flex:0.2}}/>
+//     <Text> Teste de Viewbox!</Text>
+//     </View>
+//   );
+// };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+// const  TesteTexto = () => {
+//     const [titleText, TesteTexto] = useState("Veja a mensagem [aperte aqui]");
+//     const bodyText = 'Melhor Faculdade de Tecnologia';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+//     const onPressTitle = () => {
+//       TesteTexto("FATEC ZL");
+//     };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+//     return (
+//       <Text style={styles.baseText}>
+//           <Text style={styles.titleText} onPress={onPressTitle}>
+//             {titleText}
+//             {'\n'}
+//             {'\n'}
+//           </Text>
+//           <Text numberOfLines={5}>{bodyText}</Text>
+//       </Text>
+//     );
+
+// };
+
+// const styles = StyleSheet.create({
+//   baseText: {
+//     fontFamily: 'Cochin',
+//   },
+//   titleText: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+// });
+
+// export default TesteTexto;
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+container: {
+  paddingTop: 50,
+},
+tinyLogo: {
+  width: 50,
+  height: 50,
+},
+logo: {
+  width: 66,
+  height: 58,
+},
 });
 
-export default App;
+const DisplayAnImage = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri:'https://reactnative.dev/img/tiny_logo.png'}}/>
+    </View>
+  );
+};
+
+export default DisplayAnImage;
